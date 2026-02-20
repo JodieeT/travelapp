3488a37ea85ce46c6e713722b4ec954c1538679c
JodieeT <tjodie@umich.edu>
2026-02-20 13:50:26 -0500

    Add mobile APIs

--- backend/src/routes/public.routes.js
+++ backend/src/routes/public.routes.js
@@ -5,5 +5,7 @@ router.get('/banners', controller.getBanners);
 router.get('/hotels', controller.listHotels);
 router.get('/hotels/:id', controller.getHotelDetail);
 router.get('/prices/stream', controller.streamPrices);
+router.get('/cities', controller.getCities);
+router.get('/tags', controller.getTags);
 
 module.exports = router;
